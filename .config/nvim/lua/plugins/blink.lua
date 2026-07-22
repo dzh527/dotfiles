83f76d932e83ddb5010f1afb3dac51ad15306f26
dingzhao.123 <dingzhao.123@bytedance.com>
2026-07-22 20:43:13 +0800

    tab2

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -3,7 +3,7 @@ return {
     "saghen/blink.cmp",
     opts = {
       keymap = {
-        preset = "super_tab",
+        preset = "super-tab",
         ["<Tab>"] = {
           "select_and_accept",
           "snippet_forward",
