3d4b4c4dfa616642ca0e2df2ed0657b45ea3fe59
dingzhao.123 <dingzhao.123@bytedance.com>
2026-07-27 17:12:43 +0800

    markdown preview codex panel

---
+++ .config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    keys = {
+      {
+        "<leader>ms",
+        "<cmd>Markview splitToggle<cr>",
+        desc = "Markview Split Preview",
+      },
+    },
+  },
+}
