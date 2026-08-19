3c594503422cf333c871867c7ff9e145fd598888
dingzhao.123 <dingzhao.123@bytedance.com>
2026-08-19 14:15:41 +0800

    explorer

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,8 +1,30 @@
 return {
   {
     "folke/snacks.nvim",
-    opts = {
-      scroll = { enabled = false },
-    },
+    opts = function(_, opts)
+      opts.scroll = opts.scroll or {}
+      opts.scroll.enabled = false
+
+      local git = require("snacks.explorer.git")
+      if git._clear_stale_dir_status then
+        return
+      end
+
+      local update = git._update
+      git._update = function(cwd, results)
+        local tree = require("snacks.explorer.tree")
+        local cleared = false
+
+        tree:walk(tree:find(cwd), function(node)
+          if node.dir_status ~= nil then
+            node.dir_status = nil
+            cleared = true
+          end
+        end, { all = true })
+
+        return update(cwd, results) or cleared
+      end
+      git._clear_stale_dir_status = true
+    end,
   },
 }
